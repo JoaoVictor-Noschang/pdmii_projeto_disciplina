64c53684c17fd8a164f08b199933ae50f12563c0
JoaoVictor-Noschang <joaovictornoschangsilva601@gmail.com>
2025-06-28 19:10:09 -0400

    desenvolvendo a tela de dashboard

--- src/app/(tabs)/dashboard/index.js
+++ src/app/(tabs)/dashboard/index.js
@@ -11,10 +11,34 @@ export default function AssetExample() {
 
     return (
         <View style={styles.tela}>
-            
+
             <HeaderPage />
-            
-            <Text style={styles.subtitle}>DashBoard!</Text>
+
+            <View style={styles.dashboard}>
+                <View style={styles.headRel}>
+                    <Text style={styles.title}>Relatórios</Text>
+                    <Text style={styles.title}>HOJE</Text>
+                </View>
+                <View style={styles.label}>
+                    <Text style={styles.labelText}>Consumo de Caloriass</Text>
+                    <Text style={[styles.labelInfo, { backgroundColor: '#FF5714' }]}>1.000 Kcal</Text>
+                </View>
+                <View style={styles.label}>
+                    <Text style={styles.labelText}>Consumo de Água</Text>
+                    <Text style={[styles.labelInfo, { backgroundColor: '#1BE7FF' }]}>1,5 Lt</Text>
+                </View>
+                <View style={styles.label}>
+                    <Text style={styles.labelText}>Tempo de Eexercícios</Text>
+                    <Text style={[styles.labelInfo, { backgroundColor: '#8C52FF' }]}>1h 30m</Text>
+                </View>
+                <View style={[styles.label, { marginTop: 15 }]}>
+                    <Text style={styles.labelText}>IMC Atual</Text>
+                    <View style={styles.dubLabel}>
+                        <Text style={styles.labelImc}>22,86</Text>
+                        <Text style={styles.labelImc}>Normal</Text>
+                    </View>
+                </View>
+            </View>
 
         </View>
     );
@@ -29,4 +53,46 @@ const styles = StyleSheet.create({
         paddingHorizontal: 30,
         paddingBottom: '10%',
     },
+    dashboard: {
+        padding: 25,
+        backgroundColor: '#0D3B66',
+        borderRadius: 25,
+        color: "#fff",
+        gap: 30,
+        marginVertical: 'auto',
+    },
+    headRel: {
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        marginBottom: 10,
+    },
+    title: {
+        fontWeight: 'bold',
+        fontSize: 18,
+        color: "#fff",
+    },
+    label: {
+        gap: 5
+    },
+    labelText: {
+        color: "#fff",
+    },
+    labelInfo: {
+        width: '100%',
+        borderRadius: 5,
+        padding: 10
+    },
+    dubLabel: {
+        flexDirection: 'row',
+        width: '100%',
+        justifyContent: 'space-between',
+        gap: '2%',
+    },
+    labelImc: {
+        width: '49%',
+        borderRadius: 5,
+        padding: 10,
+        backgroundColor: '#6EEB83',
+    }
 });
